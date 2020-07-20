ba518fc322163970a5c40bea625bc806309ad091
igmrrf <francis.igbiriki.fi@gmail.com>
2020-07-20 16:09:15 +0100

    This commit made changes and corrections to the configuration
    Files Modified: containers/confi.js & middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ function auth(req, res, next) {
   const token = req.header("x-auth-token");
   if (!token) return res.status(401).send("Access Denied. No token provided");
   try {
-    const decoded = jwt.verify(token, config.get("vividly_jwtkey"));
+    const decoded = jwt.verify(token, config.get("jwt_secret_key"));
     req.user = decoded;
     next();
   } catch (ex) {
