6c6ebbd955ed238646f0a4186f28a1ca80c0524a
igmrrf <francis.igbiriki.fi@gmail.com>
2020-07-20 05:03:14 +0100

    Setting Up Environmental Variables

--- containers/config.js
+++ containers/config.js
@@ -1,5 +1,17 @@
 const winston = require("winston");
+const config = require("config");
+const morgan = require("morgan");
 
-module.exports = function () {
-  winston.info("This is the config setup");
+module.exports = function (app) {
+  // * CHECKING FOR WORKING ENVIRONMENTAL VARIABLES
+  if (!config.get("password")) {
+    throw new Error("FATAL ERROR: missing environmental variable 'PASSWORD' ");
+  }
+
+  if (app.get("env") === "development") {
+    winston("Username: " + config.get("username"));
+    winston("Password: " + config.get("password"));
+    app.use(morgan("tiny"));
+    winston.info("Morgan Enabled");
+  }
 };
